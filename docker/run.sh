5ae0c763899a6283f09165309fe08f770a57830f
Xavier Blanc <xavier.blanc@labri.fr>
2017-07-10 15:13:04 +0200

    new repo

--- docker/run.sh
+++ docker/run.sh
@@ -4,6 +4,6 @@ mkdir -p work
 cd work
 git clone https://github.com/mrs-sam/page.git
 ls 
-cd mrs-sam-page
+cd page
 npm install
 xvfb-run --server-args="-screen 0 1024x768x24" node ./server.js --mongo="mongo" --rabbit="rabbit" --sftp="sftp"
